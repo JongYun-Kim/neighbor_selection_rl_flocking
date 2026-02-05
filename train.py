197ed461849f1b4af7ead195d7a00625a5b68059
jk <jongyun.kim@manchester.ac.uk>
2026-02-05 17:15:36 +0000

    Update: configurations

--- train.py
+++ train.py
@@ -17,20 +17,51 @@ if __name__ == "__main__":
     # Set up environment configuration
     default_config_path = "./envs/default_env_config.yaml"
     my_config = load_config(default_config_path)
-    # env-env config:
-    my_config.env.num_agents_pool = [8, 16]
-    my_config.env.max_time_steps = 200
-    my_config.env.use_fixed_episode_length = True
-    # my_config.env.comm_range = None
+
+    # environment configs
+    my_config.env.acs_train_w_ctrl = 0.02
+    my_config.env.acs_train_w_pos  = 1.0
+    my_config.env.acs_train_w_vel  = 0.2
+    my_config.env.action_type = "binary_vector"
+    my_config.env.agent_name_prefix = "agent_"
+    my_config.env.alignment_goal = 0.97
+    my_config.env.alignment_rate_goal = 0.03
+    my_config.env.alignment_window_length = 32
+    my_config.env.comm_range = None
+    my_config.env.dt = 0.1
+    my_config.env.entropy_p_goal = None
+    my_config.env.entropy_p_rate_goal = 0.1
+    my_config.env.entropy_rate_window_length = 50
+    my_config.env.entropy_v_goal = 0.1
+    my_config.env.entropy_v_rate_goal = 0.2
+    my_config.env.env_mode = "single_env"
+    my_config.env.get_action_hist = False
+    my_config.env.get_state_hist = False
+    my_config.env.ignore_comm_lost_agents = False
     my_config.env.is_training = True
-    # env-control config:
-    # my_config.control.speed = 15.0
-    # my_config.control.initial_position_bound = 250.0
-    # my_config.control.max_turn_rate = 15/8
+    my_config.env.max_time_steps = 1000
+    my_config.env.num_agents_pool = [20]
+    my_config.env.obs_dim = 4
+    my_config.env.periodic_boundary = False
+    my_config.env.seed = None
+    my_config.env.task_type = "acs"
+    my_config.env.use_fixed_episode_length = True
+
+    # control config:
+    my_config.control.beta = 1/3
+    my_config.control.initial_position_bound = 250.0
+    my_config.control.k1 = 1.0
+    my_config.control.k2 = 3.0
+    my_config.control.lam = 5.0
+    my_config.control.max_turn_rate = 8/15
+    my_config.control.r0 = 60.0
+    my_config.control.rho = 1.0
+    my_config.control.sig = 1.0
+    my_config.control.speed = 15.0
 
     # register your custom environment
     env_config = {
-        "seed_id": None,
+        "seed_id": 42,
         "config": my_config.dict(),  # pass dict to save the config
     }
     env_name = "neighbor_selection_flocking_env"
@@ -38,23 +69,23 @@ if __name__ == "__main__":
 
     # Set up custom model configuration
     custom_model_config = {
-        "share_layers": False,
+        "d_embed_context": 128,
+        "d_embed_input": 128,
+        "d_ff": 256,
+        "d_ff_decoder": 256,
+        "d_model": 128,
+        "d_model_decoder": 128,
         "d_subobs": 4,
-        "d_embed_input": 256,
-        "d_embed_context": 256,
-        "d_model": 256,
-        "d_model_decoder": 256,
-        "n_layers_encoder": 3,
-        "n_layers_decoder": 1,
-        "num_heads": 8,
-        "d_ff": 512,
-        "d_ff_decoder": 512,
         "dr_rate": 0,
-        "norm_eps": 1e-5,
-        "is_bias": False,  # Default is False, but True used in LazyControl
-        "use_residual_in_decoder": True,
+        "is_bias": False,
+        "n_layers_decoder": 1,
+        "n_layers_encoder": 3,
+        "norm_eps": 1e-05,
+        "num_heads": 4,
+        "scale_factor": 0.002,
+        "share_layers": False,
         "use_FNN_in_decoder": True,
-        "scale_factor": 0.05,
+        "use_residual_in_decoder": True,
     }
 
     # register your custom model
@@ -64,61 +95,39 @@ if __name__ == "__main__":
     # train
     tune.run(
         "PPO",
-        name="neighbor_selection_test_250702",
+        name="neighbor_selection_test_260205",
         # resume=True,
-        # stop={"episode_reward_mean": -101},
-        # stop={"training_iteration": 300},
-        checkpoint_freq=0,
-        keep_checkpoints_num=16,
+        checkpoint_freq=8,
+        keep_checkpoints_num=32,
         checkpoint_at_end=True,
         checkpoint_score_attr="episode_reward_mean",
         config={
             "env": env_name,
             "env_config": env_config,
             "framework": "torch",
-            #
-            # "callbacks": MyCallbacks,
-            #
             "model": {
                 "custom_model": model_name,
                 "custom_model_config": custom_model_config,
-                # "custom_action_dist": "det_cont_action_dist" if custom_model_config["use_deterministic_action_dist"] else None,
             },
-            "num_gpus": 1,
-            "num_workers": 1,
+            "num_gpus": 0.5,
+            "num_workers": 16,
             "num_envs_per_worker": 1,
-            "rollout_fragment_length": 400,
-            "train_batch_size": 400,
-            "sgd_minibatch_size": 64,
-            "num_sgd_iter": 4,
-            # "batch_mode": "complete_episodes",
-            # "batch_mode": "truncate_episodes",
-            "lr": 4e-5,
-            # "lr_schedule": [[0, 2e-5],
-            #                 [1e7, 1e-7],
-            #                 ],
-            # Must be fine-tuned when sharing vf-policy layers
-            "vf_loss_coeff": 0.25,
-            # In the...
+            "rollout_fragment_length": 1024,
+            "train_batch_size": 16384,
+            "sgd_minibatch_size": 256,
+            "num_sgd_iter": 10,
+            "lr": 2e-5,
+            "lr_schedule": [[0, 2e-5],
+                            [1e7, 1e-7],],
+            "vf_loss_coeff": 0.5,
             "use_critic": True,
             "use_gae": True,
-            "gamma": 0.991,
-            "lambda": 0.96,
-            "kl_coeff": 0,  # no PPO penalty term; we use PPO-clip anyway; if none zero, be careful Nan in tensors!
-            # "entropy_coeff": tune.grid_search([0, 0.001, 0.0025, 0.01]),
-            # "entropy_coeff_schedule": None,
-            # "entropy_coeff_schedule": [[0, 0.003],
-            #                            [5e4, 0.002],
-            #                            [1e5, 0.001],
-            #                            [2e5, 0.0005],
-            #                            [5e5, 0.0002],
-            #                            [1e6, 0.0001],
-            #                            [2e6, 0],
-            #                            ],
-            "clip_param": 0.22,  # 0.3
+            "gamma": 0.99,
+            "lambda": 0.95,
+            "kl_coeff": 0,
+            "clip_param": 0.2,
             "vf_clip_param": 256,
-            # "grad_clip": None,
-            "grad_clip": 10.0,
+            "grad_clip": 0.5,
             "kl_target": 0.01,
         },
     )
